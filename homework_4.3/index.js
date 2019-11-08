6fe935bc465a096df4e126f80ba6291b78a14845
kate-yatsenko <yatsenko.km@gmail.com>
2019-11-08 20:45:26 +0200

    finish with hometask 4.3

---
+++ homework_4.3/index.js
@@ -0,0 +1,18 @@
+const myPow = (num, deg) => {
+  let result;
+
+  if (!Number.isInteger(deg)) throw `Invalid degree: ${deg}`;
+  else if (Number.isNaN(num) || num < -100  || num > 100) {
+    throw `Invalid number: ${num}`;
+  }
+
+  if (deg === 0) result = 1;
+  else if (deg > 0) result = num * myPow(num, deg - 1);
+  else result = 1 / (num * myPow(num, -deg - 1));
+
+  return result.toFixed(5)
+};
+
+console.log(myPow(2.00000, 10));
+console.log(myPow(2.10000, 3));
+console.log(myPow(2.00000, -2));
